2ae9a492bf1f472cfb63ec4cb32218506ef2d1a1
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-12-06 13:57:43 +0800

    Initial commit

---
+++ src/main/java/com/fengxuechao/example/springcloudstreamrabbitexample/SpringCloudStreamRabbitExampleApplication.java
@@ -0,0 +1,18 @@
+package com.fengxuechao.example.springcloudstreamrabbitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Cloud Stream rabbit 示例
+ *
+ * @author fengxuechao
+ */
+@SpringBootApplication
+public class SpringCloudStreamRabbitExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamRabbitExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengxuechao/example/springcloudstreamrabbitexample/SpringCloudStreamRabbitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengxuechao.example.springcloudstreamrabbitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamRabbitExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
