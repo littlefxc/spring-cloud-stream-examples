de6179fb4ff92161e8137ee86f7ad0ce90661a86
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-12-13 14:53:21 +0800

    feat:hello world

---
+++ src/main/java/com/fengxuechao/example/rabbit/SinkReceiver.java
@@ -0,0 +1,26 @@
+package com.fengxuechao.example.rabbit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+/**
+ * {@link EnableBinding} 注解实现对消息通道的绑定<br>
+ * {@link Sink} 是 Spring Cloud Stream 中默认实现的对输入消息通道的绑定的定义<br>
+ * {@link StreamListener} 该注解表示该方法为消息中间件上数据流的事件监听器,
+ * Sink.INPUT参数表示这是input消息通道上的监听处理器
+ *
+ * @author fengxuechao
+ * @version 0.1
+ * @date 2019/12/6
+ */
+@Slf4j
+@EnableBinding(Sink.class)
+public class SinkReceiver {
+
+    @StreamListener(Sink.INPUT)
+    public void receive(Object payload) {
+        log.info("Received:{}", payload);
+    }
+}
--- src/main/java/com/fengxuechao/example/rabbit/SpringCloudStreamRabbitExampleApplication.java
+++ src/main/java/com/fengxuechao/example/rabbit/SpringCloudStreamRabbitExampleApplication.java
@@ -1,4 +1,4 @@
-package com.fengxuechao.example.springcloudstreamrabbitexample;
+package com.fengxuechao.example.rabbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
