4166240d36c53f8214c5619e4c6d5e54075f7f78
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-12-25 16:31:50 +0800

    feat(helloworld):添加 kafka 绑定

--- helloworld/src/main/java/com/fengxuechao/example/rabbit/SinkReceiver.java
+++ helloworld/src/main/java/com/fengxuechao/example/rabbit/SinkReceiver.java
@@ -1,17 +1,10 @@
 package com.fengxuechao.example.rabbit;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.integration.annotation.Transformer;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * {@link EnableBinding} 注解实现对消息通道的绑定<br>
@@ -24,18 +17,8 @@ import org.springframework.scheduling.annotation.Scheduled;
  * @date 2019/12/6
  */
 @Slf4j
-@EnableBinding({Sink.class, Source.class})
+@EnableBinding({Sink.class})
 public class SinkReceiver {
-
-    @Autowired
-    private Source source;
-
-    /**
-     * 要指定输出通道
-     */
-    @Autowired
-    @Qualifier("output")
-    private MessageChannel messageChannel;
 
     /**
      * 消费消息
@@ -45,28 +28,5 @@ public class SinkReceiver {
     @StreamListener(Sink.INPUT)
     public void receive(@Payload String payload) {
         log.info("Received:{}", payload);
-    }
-
-    /**
-     * 支持企业消息集成
-     * 处理消息
-     *
-     * @param message
-     * @return
-     */
-    @Transformer(inputChannel = Sink.INPUT, outputChannel = Source.OUTPUT)
-    public Object transform(String message) {
-        return message.toUpperCase();
-    }
-
-    /**
-     * 生产消息
-     */
-    @Scheduled(fixedRate = 2000)
-    public void sendMessage() {
-        log.info("定时发送消息");
-        // 使用 Source 和 MessageChannel 可以达到同样的效果
-        source.output().send(MessageBuilder.withPayload("hello, 123").build());
-//        messageChannel.send(MessageBuilder.withPayload("hello, 123").build());
     }
 }
---
+++ helloworld/src/main/java/com/fengxuechao/example/rabbit/WebController.java
@@ -0,0 +1,40 @@
+package com.fengxuechao.example.rabbit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fengxuechao
+ * @version 0.1
+ * @date 2019/12/25
+ */
+@EnableBinding({Source.class})
+@RestController
+public class WebController {
+
+    /**
+     * 要指定输出通道
+     */
+    @Autowired
+    @Qualifier(Source.OUTPUT)
+    private MessageChannel messageChannel;
+
+    @Autowired
+    private Source source;
+
+    @GetMapping("/messageChannel")
+    public void messageChannel() {
+        messageChannel.send(MessageBuilder.withPayload("使用 MessageChannel 发送消息").build());
+    }
+
+    @GetMapping("/source")
+    public void source() {
+        source.output().send(MessageBuilder.withPayload("使用 Source 发送消息").build());
+    }
+}
